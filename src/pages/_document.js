4427d0d6ee0e65eb113df6ef63d5ee5af6020d1a
xyzuan <xyzuannihboss@gmail.com>
2024-01-30 15:28:17 +0700

    main: Update SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,13 +10,13 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Wedding Invitation Dhody & Arisa" />
         <meta property="og:description" content="12 Februari 2024 - Desa Panjerejo - Rejotangan - Tulungagung - Jawa Timur" />
-        <meta property="og:image" content="https://dyuan.my.id/assets/gallery/DSC07639.jpg" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/87eb374b-79e4-40be-aa89-520a769e7aca.jpg?token=HrRJFnwsFNLJKZT_IC38dfAYKwcExZ4Dh7y7XewWtOc&height=1800&width=1200&expires=33242603217" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="dyuan.my.id" />
         <meta property="twitter:url" content="https://dyuan.my.id" />
         <meta name="twitter:title" content="Wedding Invitation Dhody & Arisa" />
         <meta name="twitter:description" content="12 Februari 2024 - Desa Panjerejo - Rejotangan - Tulungagung - Jawa Timur" />
-        <meta name="twitter:image" content="https://dyuan.my.id/assets/gallery/DSC07639.jpg" />
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/87eb374b-79e4-40be-aa89-520a769e7aca.jpg?token=HrRJFnwsFNLJKZT_IC38dfAYKwcExZ4Dh7y7XewWtOc&height=1800&width=1200&expires=33242603217" />
       </Head>
       <body>
         <Main />
