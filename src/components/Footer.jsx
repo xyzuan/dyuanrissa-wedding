d56a7c19374642575836e60ec0606dbd163810c5
xyzuan <xyzuannihboss@gmail.com>
2024-01-30 11:49:28 +0700

    main: Update nav z  index

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <div className="max-w-screen-sm w-fit fixed bottom-0 mb-6 mx-auto right-0 left-0 z-20">
+    <div className="max-w-screen-sm w-fit fixed bottom-0 mb-6 mx-auto right-0 left-0 z-50">
       <div className="bg-[#9a6953] backdrop-blur-xl bg-opacity-80 w-full px-6 rounded-full shadow">
         <ul className=" flex items-center gap-6 justify-evenly text-white text-xs font-light py-2 md:py-3">
           <a href="#">
